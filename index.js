6ca644e453bed51734db6d60203a2669f1989235
brunoricky <bruninhocosta94@gmail.com>
2022-06-23 04:12:40 -0300

    some corrections

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 document
   .querySelector(".img1")
-  .setAttribute("src", "images\\dice" + randomNumber1 + ".png");
+  .setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
 document
   .querySelector(".img2")
-  .setAttribute("src", "images\\dice" + randomNumber2 + ".png");
+  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
